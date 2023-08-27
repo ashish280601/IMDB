618758db81c5a5956f5f2cd72397330603a26976
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-08-27 16:03:02 +0530

    Added both html file and styling css

---
+++ Js/index.js
@@ -0,0 +1,41 @@
+const favList = [];
+const isfav = false;
+
+
+// fetching API functionality
+function imdbAPI(url){
+    // write your code here
+
+    // https://www.omdbapi.com/--->API link
+}
+
+
+
+// search functionality 
+
+function searchMovie(){
+    // write your code here
+
+}
+
+// movei pfe funtionality code
+const moviesPage = null;
+
+function showMOviePage(){
+    //write your code here
+    
+}
+
+
+
+
+// favourite funtionality code
+function addFavourite () {
+    // write code here
+
+}
+
+function removeFavourite(){
+    // write your code here
+
+}
